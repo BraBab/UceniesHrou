dba886fe00480ad0930f60d7bf5a5566ea050098
Gosha8642 <egor.saltovskI@gmail.com>
2026-02-08 12:05:37 +0100

    added new desighn

--- questions_sets/questions_set_19_6.js
+++ questions_sets/questions_set_19_6.js
@@ -1,81 +1,81 @@
-window.questions_set_19_7 = {
-    text: "Ak to fakt chcete počúvať, predovšetkým asi budete chcieť vedieť, kde som sa narodil, aké som mal detstvo, čo robili moji rodičia pred mojím narodením a podobné kraviny podľa vzoru Davida Copperfielda, ale aby som vám pravdu povedal, vôbec sa mi o tom nechce rozprávať. Je to, po prvé, otrava a po druhé, moji rodičia by najmenej dva razy za sebou zinfarktovali, keby som niečo prezradil z ich súkromného života. Na také voľačo sú veľmi citliví, najmä otec. *Inak sú celkom fajn – to nevravím – ale sú hrozne citliví.* Okrem toho vám nemienim rozprávať celý svoj mizerný životopis od a po zet. Porozprávam vám len o tých bláznivých dňoch vlani okolo Vianoc, keď som sa zosypal, musel prísť sem a dať si pohov.",
-
+window.questions_set_19_6 = {
+    "text": "Cez váš zrak ku mne sladké svetlo prúdi, ktoré už slepý zrak môj nezahliadne. Na vašich nohách nesiem bremä riadne, pod ktorým k zemi klesli by mi údy. Len s vaším krídlom vzlietam z čiernej hrudy, až do neba môj duch sa s vaším vkradne. V hmle horúco a v slnci mi je chladne, ak vaša duša príkro o mne súdi. Jedinou vôľou je mi prianie vaše, vo vašom srdci myšlienky mám skryté a z vášho dychu plynú moje vety. Som ako mesiac, ktorý oči naše nespozorujú dotiaľ na blankyte, pokým ho jasné slnce neosvieti.",
+    
     "Kosice": {
         type: "choice",
-        requireText: true,
-        question: "Ktoré lexikálne prostriedky využil Salinger v ukážke 7, aby sa priblížil študentskému prostrediu?",
+        question: "Ktorá charakteristika básnického subjektu nevystihuje jeho pocity v poslednej strofe básne?",
         choices: {
-            A: "vulgárne slová",
-            B: "hovorové slová",
-            C: "eufemizmy",
-            D: "neologizmy"
+            A: "Cíti sa ako bezvýznamný človek",
+            B: "Cíti sa ako záhadný človek",
+            C: "Cíti sa ako neviditeľný človek",
+            D: "Cíti sa ako nezaujímavý človek"
         },
-        correct: "B"
+        correct: "D"
     },
 
     "Presov": {
         type: "choice",
-        question: "Tvorbu J. D. Salingera zaraďujeme",
+        question: "Ktorým slovom označuje básnik milovanú osobu?",
         choices: {
-            A: "do realistickej literatúry",
-            B: "do literatúry po roku 1945",
-            C: "do naturalistickej literatúry",
-            D: "do medzivojnovej literatúry"
+            A: "svetlo",
+            B: "blankyt",
+            C: "zem",
+            D: "slnce"
         },
-        correct: "B"
+        correct: "D"
     },
 
     "Bratislava": {
         type: "choice",
-        question: "Ktorým slovným druhom je zvýraznené slovo v nasledujúcej vete?",
-        sentence: "Porozprávam vám len o tých bláznivých dňoch vlani *okolo* Vianoc.",
+        question: "Na ktorý zmysel čitateľa autor v básni priamo nepôsobí?",
         choices: {
-            A: "predložka",
-            B: "príslovka",
-            C: "častica",
-            D: "zámeno"
+            A: "chuť",
+            B: "zrak",
+            C: "čuch",
+            D: "hmat"
         },
-        correct: "B"
+        correct: "A"
     },
 
     "Trnava": {
         type: "choice",
-        requireText: true,
-        question: "Na základe poznania celého diela vyberte možnosť, ktorá vysvetľuje, prečo rozprávač príbehu odišiel zo školy.",
+        question: "V ktorej možnosti sa nachádza epiteton (básnický prívlastok)?",
         choices: {
-            A: "Rodičia nesúhlasili s jeho komisionálnym preskúšaním",
-            B: "Odišiel na protest proti vyučovacím metódam dejepisára",
-            C: "Musel opustiť školu pre hádku so spolužiačkou",
-            D: "Prepadol zo štyroch vyučovacích predmetov"
+            A: "duša príkro o mne súdi",
+            B: "pokým ho jasné slnce neosvieti",
+            C: "len s vaším krídlom vzlietam",
+            D: "ku mne sladké svetlo prúdi"
         },
         correct: "D"
     },
 
     "Trencin": {
         type: "choice",
-        requireText: true,
-        question: "Vo zvýraznenej vete v ukážke 7 je pomlčkami oddelená/oddelený",
+        question: "V ktorej strofe sa nachádza veršový presah?",
         choices: {
-            A: "vedľajšia veta",
-            B: "prístavok",
-            C: "elipsa",
-            D: "vsuvka"
+            A: "v prvej",
+            B: "vo štvrtej",
+            C: "v tretej",
+            D: "v druhej"
         },
         correct: "D"
     },
 
     "Nitra": {
-        type: "openAnswer",
-        requireText: true,
-        question: "Ktorý druh čísloviek alebo číslovkových výrazov sa v ukážke 7 vyskytuje opakovane?",
-        answers: ["radová", "radové"]
+        type: "fill",
+        question: "Ako sa volá básnická forma, ktorú predstavuje ukážka?",
+        answers: ["sonet"]
     },
 
     "Zilina": {
-        type: "openAnswer",
-        requireText: true,
-        question: "Ktorý typ rozprávača sa nachádza v ukážke 7?",
-        answers: ["priamy"]
+        type: "fill",
+        question: "Vypíšte z ukážky synonymum k slovu prianie.",
+        answers: ["vôľa"]
+    },
+
+    "BanskaBystrica": {
+        type: "fill",
+        question: "Podľa ktorého vzoru sa skloňuje podstatné meno slnce?",
+        answers: ["mesto"]
     }
 };
